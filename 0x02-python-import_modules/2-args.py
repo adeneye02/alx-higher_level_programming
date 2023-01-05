52fe06463198941435d1f71ccf03cec572fc1236
adeneye02 <tosinolatunbosun02@gmail.com>
2023-01-05 11:28:09 -0800

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+        nargs = len(sys.argv) - 1
+            if (nargs == 0):
+                print("{:d} arguments.".format(nargs))
+            elif (nargs == 1):
+                print("{:d} argument:".format(nargs))
+            else:
+                print("{:d} arguments:".format(nargs))
+                for i in range(len(sys.argv)):
+                    if (i == 0):
+                        continue
+                    print("{:d}: {:s}".format(i, sys.argv[i]))
