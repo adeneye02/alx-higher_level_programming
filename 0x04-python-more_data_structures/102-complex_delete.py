5cedb4526d465106a4d7eb87b26a0fe743798c15
adeneye02 <tosinolatunbosun02@gmail.com>
2023-01-12 12:41:45 -0800

    new commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    list_keys = list(a_dictionary.keys())
-    for value_dic in list_keys:
-        if value == a_dictionary.get(value_dic):
-            del a_dictionary[value_dic]
+    """
+    Deletes keys with a specific value in a dictionary
+    ...
+    Parameters
+    ----------
+    a_dictionary : dictionary
+    the given dictionary to delete value from
+    Return:
+    the new dictionary
+    """
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
             return (a_dictionary)
